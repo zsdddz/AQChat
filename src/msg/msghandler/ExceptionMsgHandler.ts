18cf2211f139c0293b25fa4a98ae8baf98295699
howcode <1051495009@qq.com>
2024-04-30 16:00:29 +0800

    feat：错误消息处理器

---
+++ src/msg/msghandler/ExceptionMsgHandler.ts
@@ -0,0 +1,22 @@
+/*
+ * @Author: howcode
+ * @Date: 2024-04-30 15:54:30
+ * @LastEditTime: 2024-04-30 15:54:45
+ */
+
+import * as AQChatMSg from '../protocol/AQChatMsgProtocol_pb';
+import AQSender from '../AQSender';
+
+export default class ExceptionMsgHandler {
+
+    handle(msgAck: AQChatMSg.default.ExceptionMsg) {
+        if(msgAck == null){
+            return;
+        }
+        AQSender.getInstance().heartbeatLoop();
+        return {
+            code:msgAck.getCode(),
+            msg:msgAck.getMsg()
+        }
+    }
+}
