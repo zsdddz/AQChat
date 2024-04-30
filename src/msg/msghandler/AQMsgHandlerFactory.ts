46c6a8db24d38c956251b7b1ad8203ae34d1a9e1
howcode <1051495009@qq.com>
2024-04-30 16:01:00 +0800

    feat：注入消息处理器

--- src/msg/msghandler/AQMsgHandlerFactory.ts
+++ src/msg/msghandler/AQMsgHandlerFactory.ts
@@ -7,6 +7,8 @@
 import * as AQChatMSg from '../protocol/AQChatMsgProtocol_pb';
 import GetStsAckHandler from './GetStsAckHandler';
 import UserLoginAckHandler from './UserLoginAckHandler';
+import CreateRoomAckHandler from './CreateRoomAckHandler';
+import ExceptionMsgHandler from './ExceptionMsgHandler';
 
 export default class AQMsgHandlerFactory {
     private static instance: AQMsgHandlerFactory = new AQMsgHandlerFactory()
@@ -20,6 +22,9 @@ export default class AQMsgHandlerFactory {
         let msgCommand = AQChatMSg.default.MsgCommand;
         this.handlerMap[msgCommand.USER_LOGIN_ACK] = new UserLoginAckHandler();
         this.handlerMap[msgCommand.GET_STS_ACK] = new GetStsAckHandler();
+        this.handlerMap[msgCommand.CREATE_ROOM_ACK] = new CreateRoomAckHandler();
+        this.handlerMap[msgCommand.EXCEPTION_MSG] = new ExceptionMsgHandler();
+        this.handlerMap[msgCommand.HEART_BEAT_ACK] = "心跳保活";
     }
 
     handle(msgCommand:number, msgBody) {
@@ -31,6 +36,9 @@ export default class AQMsgHandlerFactory {
         if(null == handler){
             console.error(`未找到消息处理器,msgCommand=${msgCommand}`);
             return;
+        }
+        if(handler == '心跳保活'){
+            return handler;
         }
         return handler.handle(msgBody);
     }
