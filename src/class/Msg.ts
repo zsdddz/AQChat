ac5c20f92313b5dd8fea62179305b22c98f2e4f7
howcode <1051495009@qq.com>
2024-05-07 16:12:50 +0800

    feat：消息类

---
+++ src/class/Msg.ts
@@ -0,0 +1,10 @@
+import User from "./User"
+import MsgTypeEnum from "../enums/MsgTypeEnum"
+
+export default interface Msg {
+  user:User;
+  roomId:number;
+  msgId?:number;
+  msgType:MsgTypeEnum;
+  msg:string;
+}
