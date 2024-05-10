711fa1d156035e9af5243800e314b497d49dc39a
howcode <1051495009@qq.com>
2024-05-10 17:58:57 +0800

    pref：优化样式

--- src/views/im/components/im-nav.vue
+++ src/views/im/components/im-nav.vue
@@ -1,21 +1,18 @@
 <template>
   <div class="nav">
-    <div v-if="userInfo.userAvatar" class="nav-my row-center">
+    <div v-if="userInfo.userAvatar" class="nav-my">
       <div class="nav-my-img" >
         <div v-html="userInfo.userAvatar"></div>
       </div>
     </div>
-    
   </div>
 </template>
 
 <script setup lang="ts">
 import useAppStore from "@/store/modules/app";
+
 const appStore = useAppStore()
-
 const userInfo = appStore.userInfo
-
-console.log(userInfo);
 
 </script>
 
@@ -23,21 +20,21 @@ console.log(userInfo);
 .nav {
   height: 100%;
   width: 8%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  position: relative;
   .nav-my {
     width: 100%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
     .nav-my-img {
-      height: 100%;
-      width: 70%;
+      height: 50px;
+      width: 50px;
       border-radius: 50%;
-      padding: 10px;
-      border-radius: 50%;
-      background: linear-gradient(315deg, @im-list-avatar-bg1, @im-list-avatar-bg2);
-      box-shadow:  -5px -5px 10px @im-list-avatar-shadow1,
-                  5px 5px 10px @im-list-avatar-shadow2;
+      margin: auto;
+      // background: linear-gradient(315deg, @im-list-avatar-bg1, @im-list-avatar-bg2);
+      // box-shadow:  -5px -5px 10px @im-list-avatar-shadow1,
+      //             5px 5px 10px @im-list-avatar-shadow2;
     }
   }
 }
