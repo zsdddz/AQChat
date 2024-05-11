d9254fe5ff25776ce7adcd16d070ba4dc70a348d
howcode <1051495009@qq.com>
2024-05-11 14:59:48 +0800

    feat：修改msgId类型

--- src/class/Msg.ts
+++ src/class/Msg.ts
@@ -1,10 +1,18 @@
+/*
+ * @Author: howcode 1051495009@qq.com
+ * @Date: 2024-05-07 15:11:20
+ * @LastEditors: howcode 1051495009@qq.com
+ * @LastEditTime: 2024-05-11 14:19:03
+ * @Description: 消息类
+ */
 import User from "./User"
 import MsgTypeEnum from "../enums/MsgTypeEnum"
 
 export default interface Msg {
   user?:User;
   roomId:number;
-  msgId?:number;
+  msgId?:string;
   msgType:MsgTypeEnum;
   msg:string;
+  msgStatus?:boolean
 }
