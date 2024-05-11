d294c04b644c052a20485f3cddd0c3e668fe0231
zsdddz <hitd@foxmail.com>
2024-05-11 20:48:44 +0800

    feat(聊天表情): 修改表情路径
    将表情资源移至OSS 删除本地资源

--- src/views/im/components/im-chat.vue
+++ src/views/im/components/im-chat.vue
@@ -64,135 +64,135 @@ const editor = inject("editor",null)
 const expressions = [
   {
     "title": "[闭嘴]",
-    "icon": "/emo/闭嘴.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/闭嘴.png"
   },
   {
     "title": "[尬笑]",
-    "icon": "/emo/尬笑.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/尬笑.png"
   },
   {
     "title": "[害怕]",
-    "icon": "/emo/害怕.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/害怕.png"
   },
   {
     "title": "[奸笑]",
-    "icon": "/emo/奸笑.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/奸笑.png"
   },
   {
     "title": "[静音]",
-    "icon": "/emo/静音.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/静音.png"
   },
   {
     "title": "[开心]",
-    "icon": "/emo/开心.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/开心.png"
   },
   {
     "title": "[口罩]",
-    "icon": "/emo/口罩.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/口罩.png"
   },
   {
     "title": "[哭]",
-    "icon": "/emo/哭.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/哭.png"
   },
   {
     "title": "[酷]",
-    "icon": "/emo/酷.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/酷.png"
   },
   {
     "title": "[流汗]",
-    "icon": "/emo/流汗.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/流汗.png"
   },
   {
     "title": "[懵B]",
-    "icon": "/emo/懵B.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/懵B.png"
   },
   {
     "title": "[迷茫]",
-    "icon": "/emo/迷茫.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/迷茫.png"
   },
   {
     "title": "[面无表情]",
-    "icon": "/emo/面无表情.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/面无表情.png"
   },
   {
     "title": "[魔鬼]",
-    "icon": "/emo/魔鬼.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/魔鬼.png"
   },
   {
     "title": "[难过]",
-    "icon": "/emo/难过.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/难过.png"
   },
   {
     "title": "[呕吐]",
-    "icon": "/emo/呕吐.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/呕吐.png"
   },
   {
     "title": "[疲惫]",
-    "icon": "/emo/疲惫.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/疲惫.png"
   },
   {
     "title": "[亲吻]",
-    "icon": "/emo/亲吻.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/亲吻.png"
   },
   {
     "title": "[热恋]",
-    "icon": "/emo/热恋.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/热恋.png"
   },
   {
     "title": "[生病]",
-    "icon": "/emo/生病.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/生病.png"
   },
   {
     "title": "[生气]",
-    "icon": "/emo/生气.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/生气.png"
   },
   {
     "title": "[受伤]",
-    "icon": "/emo/受伤.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/受伤.png"
   },
   {
     "title": "[睡觉]",
-    "icon": "/emo/睡觉.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/睡觉.png"
   },
   {
     "title": "[思考]",
-    "icon": "/emo/思考.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/思考.png"
   },
   {
     "title": "[天使]",
-    "icon": "/emo/天使.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/天使.png"
   },
   {
     "title": "[头晕]",
-    "icon": "/emo/头晕.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/头晕.png"
   },
   {
     "title": "[吐舌]",
-    "icon": "/emo/吐舌.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/吐舌.png"
   },
   {
     "title": "[笑哭]",
-    "icon": "/emo/笑哭.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/笑哭.png"
   },
   {
     "title": "[斜眼]",
-    "icon": "/emo/斜眼.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/斜眼.png"
   },
   {
     "title": "[眼红]",
-    "icon": "/emo/眼红.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/眼红.png"
   },
   {
     "title": "[眨眼]",
-    "icon": "/emo/眨眼.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/眨眼.png"
   },
   {
     "title": "[震惊]",
-    "icon": "/emo/震惊.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/震惊.png"
   },
   {
     "title": "[中毒]",
-    "icon": "/emo/中毒.png"
+    "icon": "https://aqchat.oss-cn-shenzhen.aliyuncs.com/emo/中毒.png"
   }
 ]
 
