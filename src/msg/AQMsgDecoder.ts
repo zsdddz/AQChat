f221979ae591bf5a6fb6a8ad4d2fecff214c0c30
zsdddz <hitd@foxmail.com>
2024-05-16 17:21:25 +0800

    refactor(通讯模块): 修改变量名

--- src/msg/AQMsgDecoder.ts
+++ src/msg/AQMsgDecoder.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: zsdddz
  * @Date: 2024-04-21 00:53:16
- * @LastEditTime: 2024-04-21 21:16:04
+ * @LastEditTime: 2024-05-16 16:49:13
  */
 import * as AQChatMSg from '../msg/protocol/AQChatMsgProtocol_pb';
 
@@ -13,14 +13,14 @@ export default class AQMsgDecoder {
     /**
      * 构建消息体
      * 
-     * @param nMsgCode 消息编码
-     * @param oUint8Array 字节数组
+     * @param msgCommand 消息编码
+     * @param uint8Array 字节数组
      * @return 消息体
      */
-    decode(msgCommand: number, oUint8Array: Uint8Array): any {
+    decode(msgCommand: number, uint8Array: Uint8Array): any {
         if (msgCommand < 0 || 
-            null == oUint8Array || 
-            oUint8Array.byteLength <= 0) {
+            null == uint8Array || 
+            uint8Array.byteLength <= 0) {
             return null;
         }
 
@@ -30,23 +30,23 @@ export default class AQMsgDecoder {
             console.error(`消息名称为空, msgCommand = ${msgCommand}`);
             return null;
         }
-        let oTempArray = strMsgName.split("_");
+        let tempArray = strMsgName.split("_");
         strMsgName = "";
 
-        for (let strTemp of oTempArray) {
+        for (let strTemp of tempArray) {
             strMsgName += strTemp.charAt(0) + strTemp.substr(1).toLowerCase();
         }
 
         // 获取消息类
-        let oMsgClazz = AQChatMSg.default[strMsgName];
+        let msgClazz = AQChatMSg.default[strMsgName];
 
-        if (null == oMsgClazz || 
-            "function" != typeof(oMsgClazz["deserializeBinary"])) {
-                console.error(`未找到相关的消息类, msgCode = ${msgCommand}, msgName = ${strMsgName}`);
+        if (null == msgClazz || 
+            "function" != typeof(msgClazz["deserializeBinary"])) {
+                console.error(`未找到相关的消息类, msgCommand = ${msgCommand}, msgName = ${strMsgName}`);
             return null;
         }
 
-        return oMsgClazz.deserializeBinary(oUint8Array);
+        return msgClazz.deserializeBinary(uint8Array);
     }
 
     getMessageName(value) {
