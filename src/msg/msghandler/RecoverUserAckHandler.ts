42195bb2cb4ff140931e0241d3f929b1efe79fe0
howcode <1051495009@qq.com>
2024-05-18 18:11:09 +0800

    feat：恢复房间

--- src/msg/msghandler/RecoverUserAckHandler.ts
+++ src/msg/msghandler/RecoverUserAckHandler.ts
@@ -2,7 +2,7 @@
  * @Author: howcode 1051495009@qq.com
  * @Date: 2024-05-02 12:55:23
  * @LastEditors: howcode 1051495009@qq.com
- * @LastEditTime: 2024-05-02 13:04:01
+ * @LastEditTime: 2024-05-18 17:35:45
  * @Description: 恢复用户连接消息处理器
  */
 
@@ -18,7 +18,9 @@ export default class RecoverUserAckHandler {
         }
         AQSender.getInstance().heartbeatLoop();
         return {
-            roomId:msgAck.getRoomid(),
+            roomId:msgAck.getRoom()?.getRoomid(),
+            roomName:msgAck.getRoom()?.getRoomname(),
+            roomNo:msgAck.getRoom()?.getRoomno(),
             userId:msgAck.getUserid(),
             userName:msgAck.getUsername(),
             userAvatar:msgAck.getUseravatar(),
