b746bd59fa1847c67c078ba7a3ac1e2ed5314ec4
howcode <1051495009@qq.com>
2024-05-22 10:34:01 +0800

    feat：修改消息类型枚举

--- src/enums/MsgTypeEnum.ts
+++ src/enums/MsgTypeEnum.ts
@@ -3,12 +3,14 @@ enum MsgTypeEnum {
   TEXT = 0,
   // 图片
   IMAGE = 1,
-  //录音
+  // 录音
   VOICE = 2,
-  //视频
+  // 视频
   VIDEO = 3,
+  // 文件
+  FILE = 4,
   // 提示
-  TIP = 4
+  TIP = 99
 }
 
 export default MsgTypeEnum
