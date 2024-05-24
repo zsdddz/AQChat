9d0f08a6d4b64a023f80d639262f9b20beba51df
howcode <1051495009@qq.com>
2024-05-24 10:38:41 +0800

    feat：展示用户名称

--- src/views/im/components/im-nav.vue
+++ src/views/im/components/im-nav.vue
@@ -1,9 +1,17 @@
+<!--
+ * @Author: howcode 1051495009@qq.com
+ * @Date: 2024-04-26 11:50:04
+ * @LastEditors: howcode 1051495009@qq.com
+ * @LastEditTime: 2024-05-24 10:33:32
+ * @Description: 
+-->
 <template>
   <div class="nav">
     <div v-if="userInfo.userAvatar" class="nav-my">
       <div class="nav-my-img" >
         <div v-html="userInfo.userAvatar"></div>
       </div>
+      <div class="user-name text-ellipsis" :title="userInfo.userName">{{ userInfo.userName }}</div>
     </div>
   </div>
 </template>
@@ -19,9 +27,15 @@ const userInfo = appStore.userInfo
 <style lang="less" scoped>
 .nav {
   height: 100%;
-  width: 8%;
+  width: 10%;
   position: relative;
-  // background-color: @im-nav-bg;
+  .user-name{
+    margin: 20px auto;
+    color: @txt-color;
+    font-size: 14px;
+    width: 90%;
+    text-align: center;
+  }
   .nav-my {
     width: 100%;
     position: absolute;
