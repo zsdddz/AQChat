504f27475c5c98f05b7766070eb6009153afa4d7
howcode <1051495009@qq.com>
2024-06-04 17:12:57 +0800

    feat：消息字段类型变更

--- src/class/Msg.ts
+++ src/class/Msg.ts
@@ -2,7 +2,7 @@
  * @Author: howcode 1051495009@qq.com
  * @Date: 2024-05-07 15:11:20
  * @LastEditors: howcode 1051495009@qq.com
- * @LastEditTime: 2024-05-22 11:41:14
+ * @LastEditTime: 2024-06-04 17:12:28
  * @Description: 消息类
  */
 import User from "./User"
@@ -12,7 +12,7 @@ import MsgStatusEnum from "../enums/MsgStatusEnum"
 export default interface Msg {
   user?:User;
   roomId:string;
-  msgId?:string;
+  msgId?:string|number;
   msgType:MsgTypeEnum;
   msg:string;
   msgStatus?:MsgStatusEnum;
