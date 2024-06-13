e054a4c6acb5a649fc439d73d8448a1ee8c1f619
howcode <1051495009@qq.com>
2024-06-13 14:12:25 +0800

    feat：删除路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 /*
  * @Author: howcode 1051495009@qq.com
  * @Date: 2024-04-20 18:14:45
- * @LastEditors: zsdddz hitd@foxmail.com
- * @LastEditTime: 2024-04-24 00:03:07
+ * @LastEditors: howcode 1051495009@qq.com
+ * @LastEditTime: 2024-06-12 15:56:26
  * @Description: 路由配置
  */
 import {
@@ -20,11 +20,6 @@ const routes = [
     path: "/im", 
     name: "IM", 
     component: () => import("@/views/im/index.vue"),
-  },
-  {
-    path: "/main",
-    name: "Main",
-    component: () => import("@/views/main.vue"),
   },
 ];
 
